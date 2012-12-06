da4e0f1b035baf38fe0bb5585ffd0bd64319ce00
Anuthiga Sriskanthan <anuthigasriskanthan@Hasala-Sandaruwans-MacBook.local>
2012-12-06 09:58:23 +0530

    Initial Commit

---
+++ Zing_Mobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Zing_Mobile
+//
+//  Created by Anuthiga Sriskanthan on 12/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
